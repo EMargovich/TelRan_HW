21525e6e327b9ee3b6660709ecb24586597cce4f
Evgeniy Margovich <evmargovich@gmail.com>
2025-09-26 18:41:16 +0300

    ForkJoin_Fibonacci

--- src/ForkJoin_Fibonacci/Fibonacci.java
+++ src/ForkJoin_Fibonacci/Fibonacci.java
@@ -2,6 +2,8 @@ package ForkJoin_Fibonacci;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 public class Fibonacci extends RecursiveTask<Long>  {
@@ -18,12 +20,27 @@ public class Fibonacci extends RecursiveTask<Long>  {
         System.out.println("Fibonacci = " + res + " time "
                 + (System.currentTimeMillis() - start));
 
+        start = System.currentTimeMillis();
+        res = fibStream(FIBONACCI_NUM);
+        System.out.println("Fibonacci = " + res + " time "
+                + (System.currentTimeMillis() - start));
 
         start = System.currentTimeMillis();
         res = fib(FIBONACCI_NUM);
         System.out.println("Fibonacci = " + res + " time "
                 + (System.currentTimeMillis() - start));
 
+    }
+
+    private static long fibStream(int n) {
+        long[] num = {0L, 1L};
+        long[] res = new long[1];
+        IntStream.rangeClosed(2, n).forEach((i) -> {
+            res[0] = num[0] + num[1];
+            num[0] = num[1];
+            num[1] = res[0];
+        });
+        return res[0];
     }
 
     private static long fib(int n) {
